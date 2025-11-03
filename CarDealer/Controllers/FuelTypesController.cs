53bcb83c33faab46fc171eb5f5d1fda6dafd14ad
Double_K <konstantinkolesnikov999@gmail.com>
2025-11-03 17:12:44 +0200

    Full_Data_Upload

---
+++ CarDealer/Controllers/FuelTypesController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarDealer.Data;
+using CarDealer.Models;
+
+namespace CarDealer.Controllers
+{
+    public class FuelTypesController : Controller
+    {
+        private readonly CarDealerContext _context;
+
+        public FuelTypesController(CarDealerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FuelTypes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.FuelType.ToListAsync());
+        }
+
+        // GET: FuelTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fuelType = await _context.FuelType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fuelType == null)
+            {
+                return NotFound();
+            }
+
+            return View(fuelType);
+        }
+
+        // GET: FuelTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FuelTypes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] FuelType fuelType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(fuelType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fuelType);
+        }
+
+        // GET: FuelTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fuelType = await _context.FuelType.FindAsync(id);
+            if (fuelType == null)
+            {
+                return NotFound();
+            }
+            return View(fuelType);
+        }
+
+        // POST: FuelTypes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] FuelType fuelType)
+        {
+            if (id != fuelType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fuelType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FuelTypeExists(fuelType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fuelType);
+        }
+
+        // GET: FuelTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fuelType = await _context.FuelType
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fuelType == null)
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Problem might be caused because this FuelType is currently in use, if that not the case " +
+                    "see your system administrator.";
+            }
+
+            return View(fuelType);
+        }
+
+        // POST: FuelTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                var fuelType = await _context.FuelType.FindAsync(id);
+                if (fuelType != null)
+                {
+                    _context.FuelType.Remove(fuelType);
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException /* ex */)
+            {
+                //Log the error (uncomment ex variable name and write a log.)
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+        }
+
+        private bool FuelTypeExists(int id)
+        {
+            return _context.FuelType.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CarDealer/Models/Car.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarDealer.Models;
+public enum Color
+{
+    Black,
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    White,
+    Orange
+}
+public class Car
+{
+    public int Id { get; set; }
+    public int? BrandId { get; set; }
+    public Brand? Brand { get; set; }
+    public int? FuelTypeId { get; set; }
+    public FuelType? FuelType { get; set; }
+    [Required]
+    public string Model { get; set; } = string.Empty;
+    public Color? Color { get; set; }
+    [Range(0,int.MaxValue)]
+    public int Mileage { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime ManufacturingDate { get; set; }
+    [Precision(18, 2)]
+    [DataType(DataType.Currency)]
+    public decimal? Price { get; set; }
+}
